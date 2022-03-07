a4ff55a373d1e7eae9d0e7cb7d77c4b63334a774
josh handelman <71081051+handelmanjosh@users.noreply.github.com>
2022-03-07 12:38:52 -0500

    Delete gen.py

--- gen.py
+++
@@ -1,9 +0,0 @@
-import covid_stack as stk
-
-def people_gen(population):
-    for i in range(population):
-        person = stk.person()
-        stk.people.append(person)
-
-def enviro_gen(floors, **rooms):
-    pass
